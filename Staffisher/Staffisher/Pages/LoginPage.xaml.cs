a2a1c989661188999c36434fb3ae2d6023e405ec
christian harborow <christianharborow@hotmail.co.uk>
2021-04-20 00:38:49 +0100

    Add project files.

---
+++ Staffisher/Staffisher/Pages/LoginPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Staffisher.Pages
+{
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+            Xamarin.Forms.NavigationPage.SetHasNavigationBar(this, false);
+        }
+
+        private async void OnSignUpTapped(object sender, EventArgs e)
+        {
+            Navigation.InsertPageBefore(new SignUpPage(), this);
+            await Navigation.PopAsync();
+        }
+
+        private async void OnLogInClicked(object sender, EventArgs e)
+        {
+            Navigation.InsertPageBefore(new MainPage(), this);
+            await Navigation.PopAsync();
+        }
+
+        protected override bool OnBackButtonPressed()
+        {
+            return true;
+        }
+    }
+}
---
+++ Staffisher/Staffisher/Pages/SignUpPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Staffisher.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SignUpPage : ContentPage
+    {
+        public SignUpPage()
+        {
+            InitializeComponent();
+            Xamarin.Forms.NavigationPage.SetHasNavigationBar(this, false);
+        }
+
+        private async void OnLogInTapped(object sender, EventArgs e)
+        {
+            Navigation.InsertPageBefore(new LoginPage(), this);
+            await Navigation.PopAsync();
+        }
+
+        private async void OnSignUpClicked(object sender, EventArgs e)
+        {
+            Navigation.InsertPageBefore(new MainPage(), this);
+            await Navigation.PopAsync();
+        }
+
+        protected override bool OnBackButtonPressed()
+        {
+            return true;
+        }
+    }
+}
